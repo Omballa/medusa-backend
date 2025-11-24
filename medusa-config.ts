7d4349bbe206f1f4184844cfbb347286c3231713
Brandon <williamomballa@gmail.com>
2025-11-24 14:35:31 +0300

    Deployment test 1

--- medusa-config.ts
+++ medusa-config.ts
@@ -17,5 +17,63 @@ module.exports = defineConfig({
       ssl: false,
       sslmode: "disable",
     },
+    workerMode: process.env.WORKER_MODE as
+    | "shared"
+    | "worker"
+    | "server",
+    redisUrl: process.env.REDIS_URL,
   },
+
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.ADMIN_BACKEND_URL,
+  },
+
+
+  modules: [
+    {
+      resolve: "@medusajs/medusa/caching",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/caching-redis",
+            id: "caching-redis",
+            is_default: true,
+            options: {
+              redisUrl: process.env.CACHE_REDIS_URL,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/locking",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/locking-redis",
+            id: "locking-redis",
+            is_default: true,
+            options: {
+              redisUrl: process.env.LOCKING_REDIS_URL,
+            },
+          },
+        ],
+      },
+    },
+  ]
 })
